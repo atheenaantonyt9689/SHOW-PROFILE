959510d6caeb9fbb45256f4563bb61360bc9e05f
atheenaantonyt9689 <atheenaantony8@gmail.com>
2021-01-30 12:55:22 +0530

    herokuu tseting sample

--- Registration/myapp/models.py
+++ Registration/myapp/models.py
@@ -5,9 +5,11 @@ from django.db import models
 # Create your models here.
 
 class Registration(models.Model):
-    userid = models.EmailField(max_length=35, primary_key=True)
-    password=models.CharField(max_length=20)
+    Name=models.CharField(max_length=20)
+    Email= models.EmailField(max_length=35, primary_key=True)
+    Password=models.CharField(max_length=20)
+
 
 class Login(models.Model):
-    userid = models.EmailField(max_length=20, primary_key=True)
-    password = models.CharField(max_length=20)
+    Email = models.EmailField(max_length=20, primary_key=True)
+    Password = models.CharField(max_length=20)
--- Registration/myapp/views.py
+++ Registration/myapp/views.py
@@ -1,19 +1,32 @@
 from django.shortcuts import render
-#from myapp.models import Register,Login
+#from myapp.models import *
+
 from django.http import HttpResponse
 # Create your views here.
+#from Registration.myapp.models import Registration,Login
+
+
 def Index(request):
     return render(request, "index.html")
 
 #def Register(request):
  #   return render(request, "register.html")
 
+class Registration(object):
+    pass
+
+
 def Register(request):
     if request.method == 'POST':
         login = Login()
-        login.userid = request.POST.get("Name")
-        login.password = request.POST.get("Password")
+        login.Email = request.POST.get("Email")
+        login.Password = request.POST.get("Password")
         login.save()
+        register=Registration()
+        register.Name = request.POST.get("Name")
+        register.Email = request.POST.get("Email")
+        register.Password = request.POST.get("Password")
+        register.save()
         return render(request, "index.html")
     else:
         return render(request, "register.html")
